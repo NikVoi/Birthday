16273fcdce8e09e1879559acf1ce4fc284995aa5
Nik_Voi <tivkaipivka@gmail.com>
2023-06-07 17:49:24 +0300

    add: 2 animation for pc anf mobile

--- js/source.js
+++ js/source.js
@@ -1,2 +1,16 @@
 const leftAnimation = document.querySelectorAll(".seecond__left")
-const rightAnimation = document.querySelectorAll(".seecond__right")
+const rightAnimation = document.querySelectorAll(".second__right")
+
+const width = window.innerWidth
+
+console.log(width)
+
+if (width < 1000) {
+    leftAnimation.forEach(el => {
+        el.classList.add("slideInUp")
+        console.log(el)
+    });
+    rightAnimation.forEach(el => {
+        el.classList.add("slideInUp")
+    })
+}
